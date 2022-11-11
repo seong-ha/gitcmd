dc8feae1cc641b3ef34b7c60ae0839ab17b0bd05
SeongHa <ss03050@naver.com>
2022-11-11 10:13:29 +0900

    깃이그노어할거 하고 이클립스로 커밋

---
+++ YAMYAM-1/src/main/java/com/example/yamyam/Yamyam1Application.java
@@ -0,0 +1,13 @@
+package com.example.yamyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yamyam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yamyam1Application.class, args);
+	}
+
+}
---
+++ YAMYAM-1/src/test/java/com/example/yamyam/Yamyam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yamyam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yamyam1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
